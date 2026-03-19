589538bbd74a3e9b691c0ea14b1894c358e2c229
MrDhivakar <mrdhivakar.s@gmail.com>
2026-03-19 19:18:20 +0530

    Add files via upload

--- server/utils/ai.js
+++ server/utils/ai.js
@@ -1,1 +1,24 @@
+const axios = require("axios");
 
+module.exports = async (text) => {
+  try {
+    const res = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        contents: [
+          {
+            parts: [
+              {
+                text: "Convert this into short Tamil news:\n" + text
+              }
+            ]
+          }
+        ]
+      }
+    );
+
+    return res.data.candidates[0].content.parts[0].text;
+  } catch {
+    return text;
+  }
+};
