3aa835a4ba758e2b3341ed0eb787b1be94331524
MrDhivakar <mrdhivakar.s@gmail.com>
2026-03-20 15:30:11 +0530

    Add files via upload

--- server/models/News.js
+++ server/models/News.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
 
+const newsSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  image: String,
+  createdAt: { type: Date, default: Date.now },
+  likes: { type: Number, default: 0 },
+  comments: [
+    {
+      text: String,
+      user: String
+    }
+  ]
+});
+
+module.exports = mongoose.model("News", newsSchema);
